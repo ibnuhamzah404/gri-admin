6bfd5af46042f6f3f79ea11b73e31fd55b7051d2
ibnuhamzah404 <ibnuhamzah404@gmail.com>
2023-10-01 21:18:38 +0700

    seting admin lte, tambah assets img js dan css

---
+++ app/Views/admin/dashboard/v_dashboard.php
@@ -0,0 +1,438 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>AdminLTE 3 | Dashboard</title>
+
+  <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
+
+  <link rel="stylesheet" href="plugins/fontawesome-free/css/all.min.css">
+
+  <link rel="stylesheet" href="https://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css">
+
+  <link rel="stylesheet" href="plugins/tempusdominus-bootstrap-4/css/tempusdominus-bootstrap-4.min.css">
+
+  <link rel="stylesheet" href="plugins/icheck-bootstrap/icheck-bootstrap.min.css">
+
+  <link rel="stylesheet" href="plugins/jqvmap/jqvmap.min.css">
+
+
+  <link href="<?= base_url() . 'assets/admin/css/admin-lte.css' ?>" type="text/css" rel="stylesheet">
+
+  <link rel="stylesheet" href="plugins/overlayScrollbars/css/OverlayScrollbars.min.css">
+
+  <link rel="stylesheet" href="plugins/daterangepicker/daterangepicker.css">
+
+  <link rel="stylesheet" href="plugins/summernote/summernote-bs4.min.css">
+  <script nonce="b0b97d74-9d1a-4723-8cd0-d67d971d1fbf">
+    (function(w, d) {
+      ! function(j, k, l, m) {
+        j[l] = j[l] || {};
+        j[l].executed = [];
+        j.zaraz = {
+          deferred: [],
+          listeners: []
+        };
+        j.zaraz.q = [];
+        j.zaraz._f = function(n) {
+          return async function() {
+            var o = Array.prototype.slice.call(arguments);
+            j.zaraz.q.push({
+              m: n,
+              a: o
+            })
+          }
+        };
+        for (const p of ["track", "set", "debug"]) j.zaraz[p] = j.zaraz._f(p);
+        j.zaraz.init = () => {
+          var q = k.getElementsByTagName(m)[0],
+            r = k.createElement(m),
+            s = k.getElementsByTagName("title")[0];
+          s && (j[l].t = k.getElementsByTagName("title")[0].text);
+          j[l].x = Math.random();
+          j[l].w = j.screen.width;
+          j[l].h = j.screen.height;
+          j[l].j = j.innerHeight;
+          j[l].e = j.innerWidth;
+          j[l].l = j.location.href;
+          j[l].r = k.referrer;
+          j[l].k = j.screen.colorDepth;
+          j[l].n = k.characterSet;
+          j[l].o = (new Date).getTimezoneOffset();
+          if (j.dataLayer)
+            for (const w of Object.entries(Object.entries(dataLayer).reduce(((x, y) => ({
+                ...x[1],
+                ...y[1]
+              })), {}))) zaraz.set(w[0], w[1], {
+              scope: "page"
+            });
+          j[l].q = [];
+          for (; j.zaraz.q.length;) {
+            const z = j.zaraz.q.shift();
+            j[l].q.push(z)
+          }
+          r.defer = !0;
+          for (const A of [localStorage, sessionStorage]) Object.keys(A || {}).filter((C => C.startsWith("_zaraz_"))).forEach((B => {
+            try {
+              j[l]["z_" + B.slice(7)] = JSON.parse(A.getItem(B))
+            } catch {
+              j[l]["z_" + B.slice(7)] = A.getItem(B)
+            }
+          }));
+          r.referrerPolicy = "origin";
+          r.src = "/cdn-cgi/zaraz/s.js?z=" + btoa(encodeURIComponent(JSON.stringify(j[l])));
+          q.parentNode.insertBefore(r, q)
+        };
+        ["complete", "interactive"].includes(k.readyState) ? zaraz.init() : j.addEventListener("DOMContentLoaded", zaraz.init)
+      }(w, d, "zarazData", "script");
+    })(window, document);
+  </script>
+</head>
+
+<body class="hold-transition sidebar-mini layout-fixed">
+  <div class="wrapper">
+
+    <div class="preloader flex-column justify-content-center align-items-center">
+      <img class="animation__shake" src="<?= base_url() . 'assets/admin/img/logo.webp' ?>"" alt=" AdminLTELogo" height="60" width="60">
+    </div>
+
+    <nav class="main-header navbar navbar-expand navbar-white navbar-light">
+
+      <ul class="navbar-nav">
+        <li class="nav-item">
+          <a class="nav-link" data-widget="pushmenu" href="#" role="button"><i class="fas fa-bars"></i></a>
+        </li>
+        <li class="nav-item d-none d-sm-inline-block">
+          <a href="index3.html" class="nav-link">Home</a>
+        </li>
+        <li class="nav-item d-none d-sm-inline-block">
+          <a href="#" class="nav-link">Contact</a>
+        </li>
+      </ul>
+
+      <ul class="navbar-nav ml-auto">
+
+        <li class="nav-item">
+          <a class="nav-link" data-widget="navbar-search" href="#" role="button">
+            <i class="fas fa-search"></i>
+          </a>
+          <div class="navbar-search-block">
+            <form class="form-inline">
+              <div class="input-group input-group-sm">
+                <input class="form-control form-control-navbar" type="search" placeholder="Search" aria-label="Search">
+                <div class="input-group-append">
+                  <button class="btn btn-navbar" type="submit">
+                    <i class="fas fa-search"></i>
+                  </button>
+                  <button class="btn btn-navbar" type="button" data-widget="navbar-search">
+                    <i class="fas fa-times"></i>
+                  </button>
+                </div>
+              </div>
+            </form>
+          </div>
+        </li>
+
+        <li class="nav-item dropdown">
+          <a class="nav-link" data-toggle="dropdown" href="#">
+            <i class="far fa-comments"></i>
+            <span class="badge badge-danger navbar-badge">3</span>
+          </a>
+          <div class="dropdown-menu dropdown-menu-lg dropdown-menu-right">
+            <a href="#" class="dropdown-item">
+
+              <div class="media">
+                <img src="<?= base_url() . 'assets/admin/img/user.png' ?>"" alt=" User Avatar" class="img-size-50 mr-3 img-circle">
+                <div class="media-body">
+                  <h3 class="dropdown-item-title">
+                    Brad Diesel
+                    <span class="float-right text-sm text-danger"><i class="fas fa-star"></i></span>
+                  </h3>
+                  <p class="text-sm">Call me whenever you can...</p>
+                  <p class="text-sm text-muted"><i class="far fa-clock mr-1"></i> 4 Hours Ago</p>
+                </div>
+              </div>
+
+            </a>
+            <div class="dropdown-divider"></div>
+            <a href="#" class="dropdown-item">
+
+              <div class="media">
+                <img src="dist/img/user8-128x128.jpg" alt="User Avatar" class="img-size-50 img-circle mr-3">
+                <div class="media-body">
+                  <h3 class="dropdown-item-title">
+                    John Pierce
+                    <span class="float-right text-sm text-muted"><i class="fas fa-star"></i></span>
+                  </h3>
+                  <p class="text-sm">I got your message bro</p>
+                  <p class="text-sm text-muted"><i class="far fa-clock mr-1"></i> 4 Hours Ago</p>
+                </div>
+              </div>
+
+            </a>
+            <div class="dropdown-divider"></div>
+            <a href="#" class="dropdown-item">
+
+              <div class="media">
+                <img src="dist/img/user3-128x128.jpg" alt="User Avatar" class="img-size-50 img-circle mr-3">
+                <div class="media-body">
+                  <h3 class="dropdown-item-title">
+                    Nora Silvester
+                    <span class="float-right text-sm text-warning"><i class="fas fa-star"></i></span>
+                  </h3>
+                  <p class="text-sm">The subject goes here</p>
+                  <p class="text-sm text-muted"><i class="far fa-clock mr-1"></i> 4 Hours Ago</p>
+                </div>
+              </div>
+
+            </a>
+            <div class="dropdown-divider"></div>
+            <a href="#" class="dropdown-item dropdown-footer">See All Messages</a>
+          </div>
+        </li>
+
+        <li class="nav-item dropdown">
+          <a class="nav-link" data-toggle="dropdown" href="#">
+            <i class="far fa-bell"></i>
+            <span class="badge badge-warning navbar-badge">15</span>
+          </a>
+          <div class="dropdown-menu dropdown-menu-lg dropdown-menu-right">
+            <span class="dropdown-item dropdown-header">15 Notifications</span>
+            <div class="dropdown-divider"></div>
+            <a href="#" class="dropdown-item">
+              <i class="fas fa-envelope mr-2"></i> 4 new messages
+              <span class="float-right text-muted text-sm">3 mins</span>
+            </a>
+            <div class="dropdown-divider"></div>
+            <a href="#" class="dropdown-item">
+              <i class="fas fa-users mr-2"></i> 8 friend requests
+              <span class="float-right text-muted text-sm">12 hours</span>
+            </a>
+            <div class="dropdown-divider"></div>
+            <a href="#" class="dropdown-item">
+              <i class="fas fa-file mr-2"></i> 3 new reports
+              <span class="float-right text-muted text-sm">2 days</span>
+            </a>
+            <div class="dropdown-divider"></div>
+            <a href="#" class="dropdown-item dropdown-footer">See All Notifications</a>
+          </div>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" data-widget="fullscreen" href="#" role="button">
+            <i class="fas fa-expand-arrows-alt"></i>
+          </a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" data-widget="control-sidebar" data-controlsidebar-slide="true" href="#" role="button">
+            <i class="fas fa-th-large"></i>
+          </a>
+        </li>
+      </ul>
+    </nav>
+
+
+    <aside class="main-sidebar sidebar-dark-primary elevation-4">
+
+      <a href="index3.html" class="brand-link">
+        <img src="<?= base_url() . 'assets/admin/img/logo.webp' ?>" alt=" AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
+        <span class="brand-text font-weight-light">AdminLTE 3</span>
+      </a>
+
+      <div class="sidebar">
+
+        <div class="user-panel mt-3 pb-3 mb-3 d-flex">
+          <div class="image">
+            <img src="<?= base_url() . 'assets/admin/img/user.jpg' ?>" class="img-circle elevation-2" alt="User Image">
+          </div>
+          <div class="info">
+            <a href="#" class="d-block">Alexander Pierce</a>
+          </div>
+        </div>
+
+
+
+        <nav class="mt-2">
+          <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
+
+            <li class="nav-item menu-open">
+              <a href="#" class="nav-link active">
+                <i class="nav-icon fas fa-tachometer-alt"></i>
+                <p>
+                  Dashboard
+                  <i class="right fas fa-angle-left"></i>
+                </p>
+              </a>
+
+            </li>
+            <li class="nav-item">
+              <a href="pages/widgets.html" class="nav-link">
+                <i class="nav-icon fas fa-th"></i>
+                <p>
+                  Widgets
+                  <span class="right badge badge-danger">New</span>
+                </p>
+              </a>
+            </li>
+
+
+
+
+
+
+
+
+
+          </ul>
+        </nav>
+
+      </div>
+
+    </aside>
+
+    <div class="content-wrapper">
+
+      <div class="content-header">
+        <div class="container-fluid">
+          <div class="row mb-2">
+            <div class="col-sm-6">
+              <h1 class="m-0">Dashboard</h1>
+            </div>
+            <div class="col-sm-6">
+              <ol class="breadcrumb float-sm-right">
+                <li class="breadcrumb-item"><a href="#">Home</a></li>
+                <li class="breadcrumb-item active">Dashboard v1</li>
+              </ol>
+            </div>
+          </div>
+        </div>
+      </div>
+
+
+      <section class="content">
+        <div class="container-fluid">
+
+          <div class="row">
+            <div class="col-lg-3 col-6">
+
+              <div class="small-box bg-info">
+                <div class="inner">
+                  <h3>150</h3>
+                  <p>New Orders</p>
+                </div>
+                <div class="icon">
+                  <i class="ion ion-bag"></i>
+                </div>
+                <a href="#" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
+              </div>
+            </div>
+
+            <div class="col-lg-3 col-6">
+
+              <div class="small-box bg-success">
+                <div class="inner">
+                  <h3>53<sup style="font-size: 20px">%</sup></h3>
+                  <p>Bounce Rate</p>
+                </div>
+                <div class="icon">
+                  <i class="ion ion-stats-bars"></i>
+                </div>
+                <a href="#" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
+              </div>
+            </div>
+
+            <div class="col-lg-3 col-6">
+
+              <div class="small-box bg-warning">
+                <div class="inner">
+                  <h3>44</h3>
+                  <p>User Registrations</p>
+                </div>
+                <div class="icon">
+                  <i class="ion ion-person-add"></i>
+                </div>
+                <a href="#" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
+              </div>
+            </div>
+
+            <div class="col-lg-3 col-6">
+
+              <div class="small-box bg-danger">
+                <div class="inner">
+                  <h3>65</h3>
+                  <p>Unique Visitors</p>
+                </div>
+                <div class="icon">
+                  <i class="ion ion-pie-graph"></i>
+                </div>
+                <a href="#" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
+              </div>
+            </div>
+
+          </div>
+
+
+
+
+        </div>
+      </section>
+
+    </div>
+
+    <footer class="main-footer">
+      <strong>Copyright &copy; GRI <a href="https://adminlte.io"></a>.</strong>
+      All rights reserved.
+      <div class="float-right d-none d-sm-inline-block">
+        <b>Version</b> 3.2.0
+      </div>
+    </footer>
+
+    <aside class="control-sidebar control-sidebar-dark">
+
+    </aside>
+
+  </div>
+
+  <script src="https://adminlte.io/themes/v3/plugins/jquery/jquery.min.js"> </script>
+
+
+
+  <script src="https://adminlte.io/themes/v3/dist/js/adminlte.js?v=3.2.0"> </script>
+
+  <script src="<?= base_url() . 'admin/js/jquery-min.js' ?>"></script>
+  <script src="<?= base_url() . 'admin/js/jquery-ui.js' ?>"></script>
+  <script src="plugins/jquery-ui/jquery-ui.min.js"></script>
+
+  <script>
+    $.widget.bridge('uibutton', $.ui.button)
+  </script>
+
+  <script src="<?= base_url() . 'admin/js/bootstrapjs.js' ?>"></script>
+
+  <script src="plugins/chart.js/Chart.min.js"></script>
+
+  <script src="plugins/sparklines/sparkline.js"></script>
+
+  <script src="plugins/jqvmap/jquery.vmap.min.js"></script>
+  <script src="plugins/jqvmap/maps/jquery.vmap.usa.js"></script>
+
+  <script src="plugins/jquery-knob/jquery.knob.min.js"></script>
+
+  <script src="plugins/moment/moment.min.js"></script>
+  <script src="plugins/daterangepicker/daterangepicker.js"></script>
+
+  <script src="plugins/tempusdominus-bootstrap-4/js/tempusdominus-bootstrap-4.min.js"></script>
+
+  <script src="plugins/summernote/summernote-bs4.min.js"></script>
+
+  <script src="plugins/overlayScrollbars/js/jquery.overlayScrollbars.min.js"></script>
+
+  <script src="<?= base_url() . 'admin/js/adminltejs.js' ?>"></script>
+
+  <script src="dist/js/demo.js"></script>
+
+  <script src="<?= base_url() . 'admin/js/dashboardjs.js' ?>"></script>
+</body>
+
+</html>
